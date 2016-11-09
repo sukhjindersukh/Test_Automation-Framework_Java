2df80c73ee0566c7eeeb9fe76ed392eeb5f771bb
SUKHJINDER <mrsukhjindersukh@gmail.com>
2016-11-09 18:27:59 +0530

    Run file changed

--- Run.bat
+++ Run.bat
@@ -3,6 +3,6 @@ cls
 echo "Test suite is starting"
 call mvn clean
 call mvn test
-echo Test Suite run has completed... 
-echo Please Press anything to exit :D
+echo Test Suite run has completed 
+echo Please Press anything to exit 
 Pause>Nul
