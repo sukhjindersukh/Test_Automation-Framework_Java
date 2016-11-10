abb3d81226607be0478dc8f63c6cdd5327b90c86
SUKHJINDER <mrsukhjindersukh@gmail.com>
2016-11-10 17:22:51 +0530

    Test result updae

--- Run.bat
+++ Run.bat
@@ -1,8 +1,7 @@
 @echo off
 cls
 echo "Test suite is starting"
-call mvn clean
-call mvn package
+call mvn install
 echo Test Suite run has completed... 
 echo Please Press anything to exit :D
 Pause>Nul
